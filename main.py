0cb4c1191d6a713729f2b1194a1b5fd756797bdd
pradhanprac <pradhan.prac@northeastern.edu>
2026-01-14 23:54:10 -0500

    add view endpoint to return patient data

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import json
+app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as file:
+        
+        data = json.load(file)
+    return data
+@app.get("/")
+def hello_world():
+    return {"message": "Patient Management System API"}
+
+
+@app.get("/about")
+
+def about():
+    return {"message": "A fully funcational Patient Management System API built with FastAPI."}
+
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
